c1167bb4dc941509c94c68a7744e81b1e5353e68
Engsoo Nats <40599248+engsoonats@users.noreply.github.com>
2021-01-08 22:41:01 +1100

    Update main.py

--- jungkook_bot/main.py
+++ jungkook_bot/main.py
@@ -13,4 +13,4 @@ async def greetings(ctx):
     await ctx.send("Annyeonghaseyo! (Onions on for Sale!)")
 
 
-client.run("Nzk2OTE5NTY5NTQwMzE3MTg0.X_e7Lw.qBspeNIT1kL0H47dlAVy3GXKtfI")
+client.run(REPLACE_ME)
