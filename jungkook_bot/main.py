9ced3c81d8b1ab4e92547d417b0ea1be14f91f65
Monkey Apprentice <bloonchipperengineer@gmail.com>
2021-01-09 13:59:42 +1100

    Added fantasy creator and fanfic

--- jungkook_bot/main.py
+++ jungkook_bot/main.py
@@ -1,12 +1,54 @@
 import discord
 import os
+import random
 from discord.ext import commands
-client = commands.Bot(command_prefix="*_*")
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix="_", intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
 
 
 @client.command()
 async def hello(ctx):
     await ctx.send("Annyeonghaseyo! (Onions on Sale!)")
+    print()
+
+
+@client.command()
+async def fantasy(ctx):
+    names = list()
+    for user in ctx.guild.members:
+        names.append(user.name)
+    names.remove("Jungkook Bot")
+    random.shuffle(names)
+    create_simple_fantasy(names[0])
+
+@client.command()
+async def fanfic(ctx, name):
+    # Write custom fanfics here as strings, in a list
+
+    # the structure should be:f'fanfiction{name}fanfiction',
+    fanfic = [f'{name} hopelessly fell for Jungkook.', f'Jungkook took {name} on a date to the beach.']
+    random.shuffle(fanfic)
+    await ctx.send(fanfic[0])
+
+
+def create_simple_fantasy(name):
+    verbs = ['went', 'kissed', 'felt', 'fell', 'loved']
+    random.shuffle(verbs)
+    return 'Jungkook' + name + verbs[0]
+
 
 print("Running client")
 client.run(os.environ["DISCORD_TOKEN"])
